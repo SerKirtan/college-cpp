9ff6cb8483dd77ab14688109d42f41907bb00097
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2022-04-23 10:12:40 +0530

    Update 3.c

--- Prac set 3/3.c
+++ Prac set 3/3.c
@@ -1,18 +1,18 @@
 //Set smaller number to 0 using return by reference
 #include<iostream>
 using namespace std;
-int zerosmaller(int &a, int &b)
+int& zerosmaller(int &a, int &b)
 {
     if(a<b)
-        a=0;
+        return a;
     else
-        b=0;
+        return b;
 }
 int main()
 {
     int x,y;
     cout<<"Enter two numbers : "<<endl;
     cin>>x>>y;
-    zerosmaller(x,y);
+    zerosmaller(x,y)=0;
     cout<<"New values are "<<x<<" "<<y;
 }
