806d05e2448a976dc45bf5959d83f1436b2cb8e2
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2022-04-23 09:33:44 +0530

    Create 4.c

---
+++ Prac set 4/4.c
@@ -0,0 +1,39 @@
+//Convert sec into  h,m,s
+#include<iostream>
+using namespace std;
+class time{
+    private:
+        int h=0,m,s,sec;
+    public:
+        inline int read(){
+            cin>>sec;
+        }
+        int convert(){
+            s=sec%60;
+            m=sec/60;
+            while(m>=60){
+                m=m-60;
+                h++;
+            }
+        }
+        int print(){
+            cout<<"------------------------"<<endl;
+            cout<<"Entered Time : "<<sec<<endl;
+            cout<<"Conversion [hours:mins:seconds] : "<<h<<":"<<m<<":"<<s<<endl;
+        }
+};
+int main(){
+    int i=0,n;
+    cout<<"Enter how many times you want to convert time : ";
+    cin>>n;
+    class time t[n];
+    for(;i<n;i++){
+        cout<<"----------------------------------"<<endl;
+        cout<<"Enter the time in seconds : ";
+        t[i].read();
+    }
+    for(i=0;i<n;i++){
+        t[i].convert();
+        t[i].print();
+    }
+}
