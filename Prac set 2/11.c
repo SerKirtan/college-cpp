3d4f5b055d952d225ad1d1e6ce55e7f14ff73e8b
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2022-04-24 08:11:40 +0530

    Update 11.c

--- Prac set 2/11.c
+++ Prac set 2/11.c
@@ -1,26 +1,9 @@
 // C++ program for dynamic allocation
 #include <iostream>
 using namespace std;
-
-class Entity{
-	int* ptr;
-
-public:
-    Entity(){
-		ptr = new int;
-		*ptr = 123;
-	}
-
-	void display(){
-		cout << *ptr << endl;
-        delete(ptr);
-        ptr=NULL;
-	}
-};
-
-int main()
-{
- Entity e1;
-	e1.display();
-	return 0;
+int main(){
+    int x;
+    cout<<"Enter a number : ";
+    cin>>x;
+    cout<<"Entered number is "<<x;
 }
