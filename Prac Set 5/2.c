1d2f939b188c3c7ef1090f414e662eed217ffe81
SerKirtan <82870418+SerKirtan@users.noreply.github.com>
2022-04-27 09:22:44 +0530

    Update 2.c

--- Prac Set 5/2.c
+++ Prac Set 5/2.c
@@ -1,7 +1,10 @@
-//claa book : bookname, authorname, rate, qty 
+//class book : bookname, authorname, rate, qty 
 #include<iostream>
 #include<string>
+#include<algorithm>
 using namespace std;
+static float max=0.0;
+static int count=0;
 class book{
     private:
         string bname;
@@ -9,14 +12,14 @@ class book{
         float rate;
         int qty;
     public:
+    float item_total;
     book(){
         cout<<"Object created\n";
     }
     ~book(){
         cout<<"Object destroyed\n";
     }
-        int input()
-        {
+        int input(){
             fflush(stdin);
             cout<<"------------------------------"<<endl;
             cout<<"Enter Book name : ";
@@ -25,21 +28,24 @@ class book{
             getline(cin,aname);
             cout<<"Enter rate and quantity  : ";
             cin>>rate>>qty;
+            item_total=rate*qty;
         }
-        int print()
-        {
+        int print(){
+            fflush(stdin);
             cout<<"--------------------------"<<endl;
             cout<<"Book name        : "<<bname<<endl;
             cout<<"Author name      : "<<aname<<endl;
             cout<<"Book price       : "<<rate<<endl;
             cout<<"Quantity         : "<<qty<<endl;
-            cout<<"Total Item Price : "<<qty*rate<<endl;
+            cout<<"Total Item Price : "<<rate*qty<<endl;
         }
-        inline int totalP(float &sum)
-        {
+        inline int totalP(float &sum){
             sum+=qty*rate;
         }
 };
+bool comp(const book& lhs, const book& rhs){
+            return lhs.item_total > rhs.item_total;
+        }
 int main()
 {
     int n,it=0;
@@ -50,9 +56,9 @@ int main()
     for(;it<n;it++)
         b[it].input();
     for(it=0;it<n;it++)
-    {
-        b[it].print();
         b[it].totalP(total_price);
-    }
+    sort(b, b+n, comp);
+    for(it=0;it<n;it++)
+        b[it].print();
     cout<<"Your total bill is of "<<total_price<<" Rs\n";
 }
