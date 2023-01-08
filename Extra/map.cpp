da3ef4a7e32f3f8e69c43c3c227ee55fe5721de5
Kirtan Patel <82870418+SerKirtan@users.noreply.github.com>
2023-01-08 11:40:15 +0530

    Update map.cpp

--- Extra/map.cpp
+++ Extra/map.cpp
@@ -35,3 +35,12 @@ int main()
 
   return 0;
 }
+/*begin() – Returns an iterator to the first element in the map.
+end() – Returns an iterator to the theoretical element that follows the last element in the map.
+size() – Returns the number of elements in the map.
+max_size() – Returns the maximum number of elements that the map can hold.
+empty() – Returns whether the map is empty.
+pair insert(keyvalue, mapvalue) – Adds a new element to the map.
+erase(iterator position) – Removes the element at the position pointed by the iterator.
+erase(const g)– Removes the key-value ‘g’ from the map.
+clear() – Removes all the elements from the map.*/
